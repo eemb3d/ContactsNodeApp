3c87d73e9c0a2d2f38cd311ab365ef92beecd32b
eemb3d <eemb3d@gmail.com>
2025-03-07 11:37:52 +0000

    Add missing envs on test setups and env.example file and remove version on docker compose override file

--- test/setup.ts
+++ test/setup.ts
@@ -3,5 +3,8 @@ export default () => {
     process.env.HUMAN = 'true';
     process.env.TEST_KEY = 'test';
     process.env.CDN_HOST = 'test';
+    process.env.REGION = 'test';
+    process.env.DYNAMO_ENDPOINT = 'test';
+    process.env.DYNAMO_TABLE_NAME = 'test';
     process.env.UNSANITISED_TEST_KEY = '   test      ';
 };
